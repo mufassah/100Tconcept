1a53682d944ba3d9f67581aeb05a5d80d40e43ca
brian <tgreul5@gmail.com>
2017-12-16 12:43:27 -0800

    added svg format

---
+++ src/components/Landing.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app-body">
+      <app-carousel :items="items"></app-carousel>
+      <app-main-content></app-main-content>      
+    </div>
+  </div>
+</template>
+
+<script>
+import MainContent from './MainContent'
+import AppCarousel from './AppCarousel'
+export default {
+  name: 'app',
+  components: {
+    appMainContent: MainContent,
+    AppCarousel
+  },
+  data () {
+    return {
+      items: [
+      {
+        src: '/static/images/aphromoo-news.jpg'
+      },
+      {
+        src: '/static/images/aphromoo-news.jpg'
+      },
+      {
+        src: '/static/images/aphromoo-news.jpg'
+      },
+      {
+        src: '/static/images/aphromoo-news.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.app-body {
+  width: 65%;
+  margin: auto;
+}
+
+</style>
