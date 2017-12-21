86e2bef13a0533b5b7abb6eb6b24777eb8eda10c
mufassah <austin.davis820@gmail.com>
2017-12-20 23:31:26 -0500

    Update button styles on home page

--- src/components/FeaturedNews.vue
+++ src/components/FeaturedNews.vue
@@ -15,26 +15,18 @@
 				<!-- <div class="story-avatar">
 					<img class="featured-image" :src="story.src">
 				</div> -->
-				<div class="first-story-body" @mouseover="showButton = true" @mouseleave="showButton = false" :style="{ backgroundImage: 'url(' + story.src + ')' }">
+				<div class="first-story-body" :style="{ backgroundImage: 'url(' + story.src + ')' }">
 					<div class="first-story-body-container">
 					<h2>{{story.title}}</h2>
-					<transition name="slide-button">
-					<a href="/#/news" class="story-button" v-if="showButton">Read More</a>
-					</transition>
+					<a href="/#/news" class="story-button">Read More</a>					
 					</div>
 				</div>
 				</div>
 				
 				
 			</div>
-			<div class="news-block" v-else>
-				<div class="story-avatar">
-				<img :src="story.src">
-			</div>
-			<div class="story-body">
-				<h3>{{story.title}}</h3>
-				<p>{{story.body}}</p>
-			</div>
+			<div v-else>
+			<app-home-news-block :story="story"></app-home-news-block>
 			</div>
 			
 		</div>
@@ -43,7 +35,11 @@
 </template>
 
 <script>
+import NewsBlock from './NewsBlock'
 export default {
+	components: {
+		appHomeNewsBlock: NewsBlock
+	},
 	data () {
 		return {
 			stories: [
@@ -72,8 +68,7 @@ export default {
 				title: 'Meet Pr0lly, The NA LCS Coach For 100 Thieves.',
 				body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
 			}
-			],
-			showButton: false
+			]
 		}
 	}
 }
@@ -87,42 +82,17 @@ export default {
 	background-color: #1d1d1d;
 	padding: 2em;
 }
-.first-story {
-	display: flex;
-	height: 25em;
-    background-size: cover;
-	box-shadow: 0 2px 6px 2px rgba(0,0,0,.2), 0 1px 4px 2px rgba(0,0,0,.14), 0 2px 1px 2px rgba(0,0,0,.12);
-}
 .featured-news {
 	display: flex;
 	flex-direction: column;
 	flex: 1;
 	margin-right: 1em;
 }
-.news-block {
+.first-story {
 	display: flex;
-	background-color: #1d1d1d;
-	padding: .25em;
-	margin-bottom: 1em;
-	border-left: 4px solid;
- 	padding-left: .5em;
-	height: 18em;
-  border-color: #a1131b;
-  box-shadow: 0 2px 8px rgba(0,0,0,.2), 0 2px 4px rgba(0,0,0,.14), 0 3px 1px 1px rgba(0,0,0,.12);
-}
-.story-avatar {
-	flex: 1;
-	padding: 1em;
-}
-.story-avatar img {
-	box-shadow: 0 2px 8px rgba(0,0,0,.2), 0 2px 4px rgba(0,0,0,.14), 0 3px 1px 1px rgba(0,0,0,.12);
-}
-.story-body {
-	flex: 3;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	padding: 1em;
+	height: 25em;
+    background-size: cover;
+	box-shadow: 0 2px 6px 2px rgba(0,0,0,.2), 0 1px 4px 2px rgba(0,0,0,.14), 0 2px 1px 2px rgba(0,0,0,.12);
 }
 .first-story-body {
 	flex: 3;
@@ -140,43 +110,4 @@ export default {
 	align-items: center;
 	height: 5em;
 }
-.story-button {
-    height: 2.5em;
-    background: #a1131b;
-    width: 8em;
-	text-align: center;
-    font-weight: bold !important;
-    font-size: 1.1em;
-    border-radius: 3px;
-    line-height: 2.5em;
-    color: #e0e0e0;
-    cursor: pointer;
-    -webkit-transition: all ease-in-out 200ms;
-    transition: all ease-in-out 200ms;
-}
-.story-button:hover{
-	/* box-shadow: 0 -1px 1px 1px rgba(224,224,224,.5), 0 0px 2px 3px rgba(224,224,224,.44), 0 1px 5px 3px rgba(224,224,224,.42); */
-	text-decoration: none;
-}
-
-.story-button:link {
-  text-decoration:  none;
-} 
-.story-button:visited {
-  text-decoration:  none;
-} 
-
-.slide-button-enter-active, .slide-button-leave-active {
-  transition: all .3s;
-}
-
-.slide-button-enter {
-  transform: translateY(35%);
-  opacity: 0;
-}
-.slide-button-leave-to {
-  transform: translateY(35%);
-  opacity: 0;
-}
-
 </style>
---
+++ src/components/NewsBlock.vue
@@ -0,0 +1,91 @@
+<template>
+    <div @mouseover="showButton = true" @mouseleave="showButton = false" class="news-block">
+		<div class="story-avatar">
+			<img :src="story.src">
+	    </div>
+		<div class="story-body">
+			<h3>{{story.title}}</h3>
+			<p>{{story.body}}</p>
+            <transition name="slide-button">
+			    <a href="/#/news" class="story-button" v-if="showButton">Read More</a>
+            </transition>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+    props: ['story'],
+    data() {
+        return {
+			showButton: false
+        }
+    }
+}
+</script>
+
+<style>
+.story-avatar {
+	flex: 1;
+	padding: 1em;
+}
+.story-avatar img {
+	box-shadow: 0 2px 8px rgba(0,0,0,.2), 0 2px 4px rgba(0,0,0,.14), 0 3px 1px 1px rgba(0,0,0,.12);
+}
+.story-body {
+	flex: 3;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	padding: 1em;
+}
+.story-button {
+    height: 2.5em;
+    background: #a1131b;
+    width: 8em;
+	text-align: center;
+    font-weight: bold !important;
+    font-size: 1.1em;
+    border-radius: 3px;
+    line-height: 2.5em;
+    color: #e0e0e0;
+    cursor: pointer;
+    -webkit-transition: all ease-in-out 200ms;
+    transition: all ease-in-out 200ms;
+}
+.story-button:hover{
+	/* box-shadow: 0 -1px 1px 1px rgba(224,224,224,.5), 0 0px 2px 3px rgba(224,224,224,.44), 0 1px 5px 3px rgba(224,224,224,.42); */
+	text-decoration: none;
+}
+
+.story-button:link {
+  text-decoration:  none;
+} 
+.story-button:visited {
+  text-decoration:  none;
+} 
+
+.slide-button-enter-active, .slide-button-leave-active {
+  transition: all .3s;
+}
+
+.slide-button-enter {
+  transform: translateY(35%);
+  opacity: 0;
+}
+.slide-button-leave-to {
+  transform: translateY(35%);
+  opacity: 0;
+}
+.news-block {
+	display: flex;
+	background-color: #1d1d1d;
+	padding: .25em;
+	margin-bottom: 1em;
+	border-left: 4px solid;
+ 	padding-left: .5em;
+	height: 18em;
+  border-color: #a1131b;
+  box-shadow: 0 2px 8px rgba(0,0,0,.2), 0 2px 4px rgba(0,0,0,.14), 0 3px 1px 1px rgba(0,0,0,.12);
+}
+</style>
