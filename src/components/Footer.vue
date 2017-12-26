e35626b26c6d349c5232c0bcaccbdf94b6fd5428
brian <tgreul5@gmail.com>
2017-12-26 15:57:37 -0800

    Cleaned some CSS in footer and added padding to the media query. I think we're going to have to add more of them but I'm not sure.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -25,33 +25,26 @@
           <p>Connect with us:</p>
         </div>
         <div class="sm-links">
-        <a target="_blank"
-           href="https://twitter.com/100Thieves" 
-           class="icon-link">
+        <a href="https://twitter.com/100Thieves" class="icon-link">
           <div>
-            <icon name="twitter" class="twitter sm-icon scaled"></icon>
+            <icon name="twitter" class="sm-icon"></icon>
           </div>
         </a>
-        <a target="_blank"
-           href="https://www.facebook.com/100Thieves/" 
-           class="icon-link">
+        <a href="https://www.facebook.com/100Thieves/" class="icon-link">
           <div>
-            <icon name="facebook" class="facebook sm-icon scaled"></icon>
+            <icon name="facebook" class="sm-icon"></icon>
           </div>
         </a>
-        <a target="_blank"
-           href="https://www.instagram.com/100thieves/" 
-           class="icon-link">
+        <a href="https://www.instagram.com/100thieves/" class="icon-link">
           <div>
-            <icon name="instagram" class="instagram sm-icon scaled"></icon>
+            <icon name="instagram" class="sm-icon"></icon>
           </div>
             
         </a>
-        <a target="_blank"
-           href="https://www.youtube.com/channel/UCnrX2_FoKieobtw19PiphDw" 
-           class="icon-link">
+        <a href="https://www.youtube.com/channel/UCnrX2_FoKieobtw19PiphDw" 
+            class="icon-link">
           <div>
-            <icon name="youtube" class="youtube sm-icon scaled"></icon>
+            <icon name="youtube" class="sm-icon"></icon>
           </div>
             
         </a>
@@ -80,7 +73,7 @@
     font-size: 1.1em;
 }
 .footer-links:hover {
-  color: #E42832;
+  color: #DB2F2F;
 }
 .footer-container {
   margin: auto;
@@ -126,45 +119,29 @@ footer {
   justify-content: space-around;
   flex: 1;
 }
+.sm-links:hover {
+  color: #DB2F2F;
+  transition: color .4s;
+}
 .sm-header {
   color: #e0e0e0;
   font-size: 1.3em;
   flex: 1;
-  display: flex;
+  display: inline-flex;
   justify-content: flex-end;
 }
-.scaled {
-  transform: scale(2);
-}
-
-.youtube:hover {
-  color: #FF0000;
-  transition: color .4s;
-}
-
-.instagram:hover {
-  color: #fb3958;
-  transition: color .4s;
-}
-
-.facebook:hover {
-  color: #3b5998;
-  transition: color .4s;
-}
-
-.twitter:hover {
-  color: #00aced;
-  transition: color .4s;
-}
-
 .sm-icon {
   position: relative;
   top: 5px;
   /* width: 50px; */
   color: #e0e0e0;
   transition: color .4s;
+  transform: scale(2);
 }
-
+.sm-icon:hover {
+  color: #E42832;
+  transition: color .4s;
+}
 
 @media (max-width: 850px) {
   .footer-container {
@@ -177,6 +154,9 @@ footer {
   }
   .sm-header {
     justify-content: center;
+  }
+  .sm-links {
+    padding: 0em 0em 1.5em 0em;
   }
 }
 </style>
