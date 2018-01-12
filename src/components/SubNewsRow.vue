74d42e76aac36a69ebfd24c918c331915106edc5
brian <tgreul5@gmail.com>
2018-01-11 16:41:20 -0800

    unimportant message

--- src/components/SubNewsRow.vue
+++ src/components/SubNewsRow.vue
@@ -17,20 +17,20 @@ export default {
 
 <style>
 .sub-news-row {
-    display: grid;
-    grid-gap: 2em 2em;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    grid-auto-rows: 400px;
-    height: 100%;
+    display: flex;
+    justify-content: space-between;
     width: 100%;
+    max-width: 88.3em;
 }
 
 .sub-news-body {
     background-color: #1d1d1d;
-    padding: 4em;
+    padding: 2em;
     box-shadow: 0 5px 10px 1px rgba(0,0,0,0.25), 0 3px 14px 2px rgba(0,0,0,0.24), 0 5px 5px -3px rgba(0,0,0,0.3);
-    /*padding-bottom:30%;
-    margin: 0em 0em 4em 0em;*/
+    height: 0; 
+    width:30%;
+    padding-bottom:30%; 
+    margin: 0 .3em 1em .3em;
     flex: 1;
     background-size: cover;
     position: relative;
